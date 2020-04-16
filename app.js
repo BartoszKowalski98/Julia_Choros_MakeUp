dc8cf0a88b72cfd69f5684b284d182a8579d9413
BartoszKowalski98 <bartosz.kowalski998@gmail.com>
2020-04-16 15:11:21 +0200

    Add new files

---
+++ app.js
@@ -0,0 +1,139 @@
+const sr = ScrollReveal();
+
+sr.reveal('.image', {
+    duration: 1800,
+    origin: 'left',
+    distance: '150px',
+    delay: 275,
+
+
+});
+
+sr.reveal('.description', {
+    duration: 1800,
+    scale: 0.5,
+    delay: 400,
+
+
+});
+
+
+sr.reveal('.julia', {
+    duration: 1800,
+    delay: 175,
+
+});
+
+sr.reveal('.okolicznosciowy', {
+
+    origin: 'left',
+    distance: '150px',
+    duration: 2500,
+});
+
+sr.reveal('.dzienny', {
+    origin: 'bottom',
+    distance: '400px',
+    duration: 2500,
+
+});
+
+sr.reveal('.wieczorowy', {
+    origin: 'right',
+    distance: '150px',
+    duration: 2500,
+});
+
+sr.reveal('.ofertah1', {
+    origin: 'left',
+    distance: '600px',
+    duration: 2500,
+});
+
+sr.reveal('.cennik', {
+    origin: 'left',
+    distance: '600px',
+    duration: 2500,
+});
+sr.reveal('.cenaokolicznosciowy', {
+    origin: 'left',
+    distance: '600px',
+    duration: 2500,
+    delay: 1350,
+});
+
+sr.reveal('.cenadzienny', {
+    origin: 'right',
+    distance: '600px',
+    duration: 2500,
+    delay: 1100,
+});
+
+
+
+sr.reveal('.cenawieczorowy', {
+    origin: 'left',
+    distance: '600px',
+    duration: 2500,
+    delay: 1100,
+});
+
+
+sr.reveal('.h1cennik', {
+    origin: 'top',
+    distance: '200px',
+    duration: 2500,
+    delay: 1100,
+});
+
+
+sr.reveal('.kontakt', {
+    origin: 'right',
+    distance: '600px',
+    duration: 2500,
+
+});
+
+sr.reveal('.kontaktfb', {
+    origin: 'top',
+    distance: '200px',
+    duration: 2500,
+    delay: 1100,
+
+});
+
+
+sr.reveal('.kontaktig', {
+    origin: 'left',
+    distance: '600px',
+    duration: 2500,
+    delay: 1100,
+
+});
+
+
+sr.reveal('.kontaktmail', {
+    origin: 'bottom',
+    distance: '200px',
+    duration: 2500,
+    delay: 1100,
+
+});
+
+
+sr.reveal('.h1kontakt', {
+    origin: 'bottom',
+    distance: '200px',
+    duration: 2500,
+    delay: 1100,
+
+});
+
+
+sr.reveal('.fota', {
+    origin: 'right',
+    distance: '600px',
+    duration: 2500,
+    delay: 1100,
+
+});
